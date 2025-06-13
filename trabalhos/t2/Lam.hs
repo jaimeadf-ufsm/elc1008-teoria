baaf2b1d035296e99b27faa1ce48eac79b3985e0
Jaime Daniel <jaimeadanielfilho@hotmail.com>
2025-06-13 00:38:54 -0300

    feat(trabalhos/t2): implement call by value with de bruijin indices

--- trabalhos/t2/Lam.hs
+++ trabalhos/t2/Lam.hs
@@ -1,83 +1,103 @@
 -- Código 10/06/2025
-module Lam where
+module Main where
 
-import Data.List
-
+import Data.Maybe as Maybe
+import Data.List as List
 
 --exemplo
-data Color = Red | Blue | Green deriving (Show, Eq, Ord)
-
-
 -- AST do Calculo-lambda
+data TLam = Var Char
+          | Abs Char TLam
+          | App TLam TLam
+        deriving (Eq, Show)
 
-data TLam = Var Char -- variavel
-          | Abs Char TLam -- abstracao
-          | App TLam TLam -- aplicacao 
-
-
-
--- ToDo: Implementar a funcao abaixo: recebe um termo e retorna uma 
--- lista de variaveis livres.
-
-freeVars :: TLam -> [Char]          
-freeVars (Var x)     = undefined
-freeVars (Abs x t)   = undefined
-freeVars (App t1 t2) = undefined
-
-
--- ToDo: Implementar a função de substituição
-subs :: Char -> TLam -> TLam -> TLam
-subs x s (Var y) = if (x == y) then s else (Var y)
-subs x s (Abs y t12) = undefined
-subs x s (App t1 t2) = undefined
-
--- ToDo: Implementar a função isVal: recebe um termo lambda e 
--- retorna True se é um valor, i.e, se não é aplicação.
-
-isVal :: TLam -> Bool
-isVal (Var x) = True
-isVal _       = undefined
--- ToDo: Implementar a semântica operacional call-by-value: um 
--- passo de beta redução
-
-eval :: TLam -> TLam
-eval (Var x) = Var x 
-eval (Abs x t) = Abs x t
-eval (App (Abs x t12) t2) = if (isVal t2) then (subs x t2 t12)
-                                          else let t2' = eval t2
-                                               in (App (Abs x t12) t2')
-eval _ = undefined                                               
-
--- aplicando varios passos do eval até que não tenha mais redex: 
---eval recursivo
---evaluation :: TLam -> TLam
-
--- Teremos uma representacao para termos sem nome
-data TNameL = VarN Int 
+data TNameL = VarN Int
             | AbsN TNameL
-            | AppN TNameL TNameL deriving (Eq, Show)
+            | AppN TNameL TNameL
+            deriving (Eq, Show)
+        
+exC0 :: TLam
+exC0 = Abs 's' (Abs 'z' (Var 'z'))
 
+exC1 :: TLam
+exC1 = Abs 's' (Abs 'z' (App (Var 's') (Var 'z')))
 
--- Contexto Gamma de variáveis livres
+exC2 :: TLam
+exC2 = Abs 's' (Abs 'z' (App (Var 's') (App (Var 's') (Var 'z'))))
 
-type Gamma =  [(Char, Int)]
-gamma1 :: Gamma 
-gamma1 = [('x',4),('y',3),('z',2),('a',1),('b',0)]
+exPlus :: TLam
+exPlus = Abs 'm' (Abs 'n' (Abs 's' (Abs 'z' (App (App (Var 'm') (Var 's')) (App (App (Var 'n') (Var 's')) (Var 'z'))))))
 
---ToDo
--- Definir uma função removeNames, que recebe um contexto de nomes 
--- gamma e um termo lambda com nomes, tal que as variaveis livres do 
--- termo devem estar em gamma. A função deve retornar o termo 
--- nameless correspondente
+exC1PlusC0 :: TLam
+exC1PlusC0 = App (App exPlus exC1) exC0
 
+exC1PlusC1 :: TLam
+exC1PlusC1 = App (App exPlus exC1) exC1
+
+type Gamma = [Char]
+
+-- encontra as variáveis livres de um termo
+freeVariables :: TLam -> [Char]
+freeVariables (Var x) = [x]
+freeVariables (Abs x t) = List.delete x (freeVariables t)
+freeVariables (App t1 t2) = freeVariables t1 `union` freeVariables t2
+
+-- substitui as variáveis por índices de bruijin
 removeNames :: Gamma -> TLam -> TNameL
-removeNames = undefined
+removeNames gamma (Var x) = VarN (Maybe.fromJust (List.elemIndex x gamma))
+removeNames gamma (Abs x t) = AbsN (removeNames (x:gamma) t)
+removeNames gamma (App t1 t2) = AppN (removeNames gamma t1) (removeNames gamma t2)
 
--- ToDo
--- Definir uma função restoreNames, que recebe um termo lambda sem 
--- nomes e um contexto gamma e produz um termo lambda com nome.
--- Para fazer isso, precisamos escolher nomes para as variáveis ligadas.
--- Podemos inicialmente escolher qualquer nome que não aparece em gamma.
-
+-- restaura os nomes das variáveis a partir dos índices de bruijin
 restoreNames :: Gamma -> TNameL -> TLam
-restoreNames = undefined
+restoreNames gamma (VarN x) = Var (gamma !! x)
+restoreNames gamma (AbsN t) = let x = head (filter (`notElem` gamma) ['a'..'z'])
+                              in Abs x (restoreNames (x:gamma) t)
+restoreNames gamma (AppN t1 t2) = App (restoreNames gamma t1) (restoreNames gamma t2)
+
+-- desloca as variáveis de um termo t por d se d >= c
+shift :: Int -> Int -> TNameL -> TNameL
+shift d c (VarN x) = if x >= c then VarN (x + d) else VarN x
+shift d c (AbsN t) = AbsN (shift d (c + 1) t)
+shift d c (AppN t1 t2) = AppN (shift d c t1) (shift d c t2)
+
+-- substitui a variável x por s no termo t
+subst :: Int -> TNameL -> TNameL -> TNameL
+subst x s (VarN y) = if x == y then s else VarN y
+subst x s (AbsN t) = AbsN (subst (x + 1) (shift 1 0 s) t)
+subst x s (AppN t1 t2) = AppN (subst x s t1) (subst x s t2)
+
+-- verifica se o termo é um valor, ou seja, não é uma aplicação
+isValue :: TNameL -> Bool
+isValue (VarN _) = True
+isValue (AbsN _) = True
+isValue (AppN _ _) = False
+
+-- avalia um termo, aplicando a semântica operacional call-by-value
+eval :: TNameL -> TNameL
+eval (AppN (AbsN t12) t2)
+    | isValue t2 = shift (-1) 0 (subst 0 (shift 1 0 t2) t12)
+eval (AppN t1 t2)
+    | isValue t1 = AppN t1 (eval t2)
+    | otherwise = AppN (eval t1) t2
+eval (AbsN t) = AbsN (eval t)
+eval (VarN x) = VarN x
+
+-- avalia um termo até que não haja mais redex
+evalAll :: TNameL -> TNameL
+evalAll t =
+    let t' = eval t
+    in if t' == t then t else evalAll t'
+
+-- executa a avaliação de um termo, restaurando os nomes das variáveis
+run :: TLam -> TLam
+run term = 
+    let gamma = freeVariables term
+        namelessTerm = removeNames gamma term
+        evaluatedTerm = evalAll namelessTerm
+    in restoreNames gamma evaluatedTerm
+
+main :: IO()
+main = do
+    let result = run exC1PlusC1
+    print result
